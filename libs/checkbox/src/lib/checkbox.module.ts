10eecd6b6a531206bf28a9ece17463e8081f69f5
Aleksey Belan <alexey.belan@spryker.com>
2020-02-03 14:24:30 +0200

    Fix modules

--- libs/checkbox/src/lib/checkbox.module.ts
+++ libs/checkbox/src/lib/checkbox.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckboxComponent } from './checkbox/checkbox.component';
-import {FormsModule} from "@angular/forms";
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule],
   exports: [CheckboxComponent],
   declarations: [CheckboxComponent]
 })
